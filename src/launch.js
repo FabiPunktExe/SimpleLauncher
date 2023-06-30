436defc064d7608e742e743d9c89bbc98a26f1b5
MrStupsi <MrStupsi@gmx.de>
2023-06-30 21:18:44 +0200

    Made download async in Thread

--- src/launch.js
+++ src/launch.js
@@ -1,17 +1,15 @@
-const { resolve, join } = require('path')
-const { Worker } = require('worker_threads')
+const { Worker, parentPort } = require('worker_threads')
 const { isMainThread, workerData } = require('worker_threads')
 
-//if (!isMainThread) require('./minecraft').launch(workerData.version, workerData.account, status => parentPort.postMessage(status))
+if (!isMainThread) require('./minecraft').launch(workerData.version, workerData.account, status => parentPort.postMessage(status))
 
 const launch = async (version, account, statusCallback) => {
-    /*new Worker(__filename, {
+    new Worker(__filename, {
         workerData: {
             version: version,
             account: account
         },
-    }).on('message', statusCallback)*/
-    setTimeout(() => require('./minecraft').launch(version, account, statusCallback))
+    }).on('message', statusCallback)
 }
 
 module.exports = {launch}
