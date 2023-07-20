49ac38de896013841570f4cb8f3057488e2afe28
MrStupsi <MrStupsi@gmx.de>
2023-07-20 21:38:15 +0200

    Fixed Optifine downloader

--- src/download/moddownloader.js
+++ src/download/moddownloader.js
@@ -6,7 +6,10 @@ const log = (...data) => console.log('[Mods Download] ' + data)
 
 const downloadMods = (version, callback) => {
     const dir = join(getDirectory(), 'versions', version.id, 'mods')
-    const mods = version.mods.map(mod => [mod, join(dir, mod.split('/')[mod.split('/').length - 1])])
+    const mods = version.mods.map(mod => {
+        if (typeof mod === 'string') return [mod, join(dir, mod.split('/')[mod.split('/').length - 1])]
+        else return [mod.url, join(dir, mod.filename)]
+    })
     downloadFilesAsync(mods, undefined, log, callback)
 }
 
